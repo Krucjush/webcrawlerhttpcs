c96f0412d0f19682c0dcce8d247d5ab01e8dafd1
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-05-25 21:08:05 +0200

    add normalizeURL

---
+++ webcrawlerhttp/Crawl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webcrawlerhttp
+{
+	public class Crawl
+	{
+		public string normalizeURL(string urlString)
+		{
+			var urlObj = new Uri(urlString);
+			var hostPath = $"{urlObj.Host}{urlObj.AbsolutePath}";
+			if (hostPath.Length > 0 && hostPath.Substring(hostPath.Length - 1, 1)[0] == '/')
+			{
+				return hostPath.Substring(0, hostPath.Length-1);
+			}
+			return hostPath;
+		}
+	}
+}
